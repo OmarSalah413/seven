00be36bf3411d535ecfe6e1b5f625995b647d387
omar.m.salah.b <73302960+OmarSalah413@users.noreply.github.com>
2020-10-22 17:43:26 +0100

    the program
    >>python3 SeVen.py
    to run the script

---
+++ SeVen.py
@@ -0,0 +1,33 @@
+#####librairies#####
+
+import socket
+
+#####logo#####
+
+print(' _____   _____   _     _   _____   __   _ ')
+print('/  ___/ | ____| | |   / / | ____| |  \ | |')
+print('| |___  | |__   | |  / /  | |__   |   \| |')
+print('\___  \ |  __|  | | / /   |  __|  | |\   |')
+print(' ___| | | |___  | |/ /    | |___  | | \  |')
+print('/_____/ |_____| |___/     |_____| |_|  \_|')
+
+#####yeah#####
+
+print("-"*42)
+
+####script####
+
+def main():
+    remote_host = input('the target please :')
+
+    try:
+        print("IP Address Of " + remote_host + " is " + socket.gethostbyname(remote_host))
+
+    except socket.error as e:
+        print("Error " , e)
+
+
+
+
+if __name__ == "__main__":
+    main()
